39a5885fd5f361fd507080c8465350bcc9813d89
EliasR07 <eliasalejo01@gmail.com>
2024-08-22 11:11:20 -0400

    Adding some modification in text sizes and designs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,23 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {},
+  theme: {
+    fontSize: {
+      xs: "0.75rem",
+      sm: "0.8rem",
+      tiny: "0.625rem",
+      base: "0.85rem",
+      lg: "1rem",
+      xl: "1.25rem",
+
+      xsv: "0.8vmax",
+      smv: "0.85vmax",
+      tinyv: "0.75vmax",
+      basev: "0.9vmax",
+      lgv: "1vmax",
+      xlv: "1.25vmax",
+    },
+  },
   fontFamily: {
     poppins: ["Avenir", "sans-serif"],
     goudy: ["Goudy", "sans-serif"],
